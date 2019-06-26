0db68e8e80408a2fcdd737b8cab10ca322e471c6
MostafaIn <mostafa.hazareh@integrify.fi>
2019-06-26 15:53:29 +0300

    added homepage branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div>
       <h1>Integrify - Student Assessment System</h1>
       <h2>Feature One Added By Husham</h2>
+      <h3> add homepage branch by Mostafa.</h3>
     </div>
   );
 }
